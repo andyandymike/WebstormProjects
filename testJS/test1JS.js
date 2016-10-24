0faccccfe70c2696fef311028520c227fc63082b
andyandymike <564458508@qq.com>
2016-10-24 21:14:22 +0800

    first commit

---
+++ testJS/test1JS.js
@@ -0,0 +1,8 @@
+/**
+ * Created by I067382 on 8/30/2016.
+ */
+
+var bResult = "a" < 3;
+alert(bResult);
+var bResult = "a" >= 3;
+alert(bResult);
---
+++ testJS/testJS.js
@@ -0,0 +1,14 @@
+/**
+ * Created by I067382 on 8/24/2016.
+ */
+
+var i;
+var cars = new Array();
+cars[0] = "Audi";
+cars[1] = "BMW";
+cars[2] = "Volvo";
+
+for (i=0;i<cars.length;i++)
+{
+    document.write(cars[i] + "<br>");
+}
