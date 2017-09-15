9ed61711136159a93ea1417e6c15c0949be16a41
Andy Lu <andy.lu@sap.com>
2017-09-15 16:02:38 +0800

    work update

--- testJS/test1JS.js
+++ testJS/test1JS.js
@@ -2,10 +2,6 @@
  * Created by I067382 on 8/30/2016.
  */
 
-async function series() {
-    await wait(500);
-    await wait(500);
-    return "done!";
-}
+'use strict';
 
-series();
+console.log("foo".repeat(3));
